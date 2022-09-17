1734ca5284ce29dec8accd4b7d869bb407251a61
jusmepaul8@gmail.com <jusmepaul8@gmail.com>
2022-09-17 13:16:00 +0100

    Deleted the logo and changed the file to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p></p>
+                <p>Hello Impractical Hackers</p>
             </header>
         </div>
     );
