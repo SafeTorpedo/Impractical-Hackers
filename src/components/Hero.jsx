08f1d5d9fb257450e46af2349e888327fb2b8977
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-09-18 01:17:57 +0530

    Added hero component, project description and button link left

---
+++ src/components/Hero.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Hero = () => {
+    return (
+        <div className="text-white">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+                <p className="text-[#00df8c] font-bold p-3">
+                    INSERT PROJECT TAGLINE
+                </p>
+                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+                    Project Info
+                </h1>
+                <div className="flex justify-center items-center py-4">
+                    <p className="md:text-4xl sm:text-3xl text-xl font-bold">
+                        Translate lyrics to
+                    </p>
+                    <Typed
+                        className="md:text-4xl sm:text-3xl text-xl font-bold md:pl-2 pl-2"
+                        strings={[
+                            "Japanese",
+                            "Chinese",
+                            "Spanish",
+                            "German",
+                            "your own local language",
+                        ]}
+                        typeSpeed={60}
+                        backSpeed={60}
+                        loop
+                    />
+                </div>
+                <p className="md:text-2xl text-xl font-bold text-gray-400">
+                    Brief project description
+                </p>
+                <button className="bg-[#00df8c] w-[180px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+                    Check it out!
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
