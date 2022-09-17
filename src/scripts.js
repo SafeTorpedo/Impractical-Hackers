6f0c3a33a5188308fa2782a51bbc87dfd20a67f6
Kaushik-Iyer <84177184+Kaushik-Iyer@users.noreply.github.com>
2022-09-17 14:06:12 +0530

    Adding file for API calls

---
+++ src/scripts.js
@@ -0,0 +1,34 @@
+
+
+const { translate } = require('bing-translate-api');
+
+var json=require('./lang.json')
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1 / ++count)
+            result = prop;
+    return result;
+}
+
+language=pickRandomProperty(json)
+music = require('musicmatch')({ apikey: "704c4ad833d840da8744b272d0a1df04" });
+
+music.matcherLyrics({ q_track: "Closer", q_artist: "Chainsmokers" })
+    .then(function (data) {
+        var lyrics  = data.message.body.lyrics.lyrics_body
+        translate(lyrics.slice(0,1000), 'en', language, true).then(res => {
+            console.log(res.translation);
+        }).catch(err => {
+            console.error(err);
+        });
+    }).catch(function (err) {
+        console.log(err);
+    })
+
+// translate(lyrics, 'en', 'ru', true).then(res => {
+//     console.log(res.translation);
+// }).catch(err => {
+//     console.error(err);
+// });
