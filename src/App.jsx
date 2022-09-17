559860706b30e1d8f0858ceb631b206daf5a453c
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-09-18 02:41:41 +0530

    Removed extra import of Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import Form from "./components/Form";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Form from "./components/Form"
+import Form from "./components/Form";
 
 function App() {
     return (
