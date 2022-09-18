1648fc28f4b37b8cbe1bf9d885481f247587382b
Kaushik-Iyer <84177184+Kaushik-Iyer@users.noreply.github.com>
2022-09-18 21:39:48 +0530

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,10 @@
 
 
+
 const { translate } = require('bing-translate-api');
 
-var json=require('./lang.json')
+
+var json = require('./lang.json')
 function pickRandomProperty(obj) {
     var result;
     var count = 0;
@@ -12,21 +14,27 @@ function pickRandomProperty(obj) {
     return result;
 }
 
-language=pickRandomProperty(json)
-music = require('musicmatch')({ apikey: {API_KEY} });
 
-music.matcherLyrics({ q_track: "Closer", q_artist: "Chainsmokers" })
+
+language = pickRandomProperty(json)
+music = require('musicmatch')({ apikey: {API_KEY});
+music.matcherLyrics({ q_track: "hey jude", q_artist: "beatles" })
     .then(function (data) {
-        var lyrics  = data.message.body.lyrics.lyrics_body
-        translate(lyrics.slice(0,1000), 'en', language, true).then(res => {
-            console.log(res.translation);
+        var lyrics = data.message.body.lyrics.lyrics_body
+        translate(lyrics.slice(0, 1000), 'en', language, true).then(res => {
+            const googleTTS = require('google-tts-api');
+            const url = googleTTS.getAudioUrl(res.translation.slice(0, 200), {
+                lang: 'en',
+                slow: false,
+                host: 'https://translate.google.com',
+            });
+            console.log(url);
         }).catch(err => {
             console.error(err);
         });
     }).catch(function (err) {
         console.log(err);
     })
-
 // translate(lyrics, 'en', 'ru', true).then(res => {
 //     console.log(res.translation);
 // }).catch(err => {
